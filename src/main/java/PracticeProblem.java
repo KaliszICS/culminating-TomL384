fd0e1152f06dd43e5411f950d1a4144f85c1b54b
TomL384 <350286454@tdsb.ca>
2026-06-10 18:23:19 +0000

    Final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,6 +7,7 @@
 
 import java.util.Scanner;
 import java.util.Random;
+import java.text.DecimalFormat;
 import java.util.ArrayDeque;
 
 public class PracticeProblem {
@@ -17,9 +18,11 @@ public class PracticeProblem {
 
 		System.out.println("Welcome to the Reverse - Cal Game!");
 
+		// ---------------- INPUT: GAME SETTINGS ----------------
+
 		System.out.print("\nPlease enter the number of rounds: ");
 
-		// Validate integer input
+		// Validate integer input for rounds
 		while (!(ip.hasNextInt())) {
 			ip.nextLine();
 			System.out.println("Invalid Input!");
@@ -43,8 +46,9 @@ public class PracticeProblem {
 		}
 
 		System.out.println("\nPlease be awared that the following settings/input will apply to all rounds/generated questions.\n");
+		System.out.println("Player answers are checked based on their numerical value, not their formatting. If two answers represent the same numerical value,   they are both considered correct. This includes standard decimal notation and scientific notation.\n");
 
-
+		// Base number input
 		System.out.print("Please enter the first number you want in the question: ");
 
 		// Validate number input
@@ -57,7 +61,7 @@ public class PracticeProblem {
 		double base = ip.nextDouble();
 		ip.nextLine();
 		
-
+		// Number of values per question
 		System.out.print("\nPlease enter the number of numbers used in each calculation/operation: ");
 
 		// Validate integer input
@@ -83,6 +87,7 @@ public class PracticeProblem {
 			ip.nextLine();
 		}
 
+		// Operation selection
 		System.out.println("\nPlease enter number 1 - 5 to select the type of operation: ");
 		System.out.println("1. Addition");
 		System.out.println("2. Subtraction");
@@ -127,58 +132,62 @@ public class PracticeProblem {
 			ip.nextLine();
 		}
 
+		// ---------------- GAME LOOP ----------------
 		int score = 0;
 		for(int i = 0; i < rounds; i++){
 			int mixed = 0;
 			int round = i + 1;
 			System.out.print("Question" + round + ": " + base);
 			
+			// Stack simulation using deque (numbers + operators)
 			ArrayDeque<Double> numbers = new ArrayDeque<>();
 			ArrayDeque<Character> operators = new ArrayDeque<>();
 			numbers.push(base);
 
+			// ---------------- QUESTION GENERATION ----------------
 			for (int i1 = 0; i1 < total - 1; i1++){
 				double randomNum = -10000 + r.nextInt(20001);
-				while(randomNum > -0.5 && randomNum < 0.5){
+
+				// prevent zero (avoids division issues)
+				while(randomNum == 0){
 					randomNum = -10000 + r.nextInt(20001);
 				}
-				double randomNum1 = Math.round(randomNum);
-				numbers.push(randomNum1);
+
+				numbers.push(randomNum);
 				mixed = 1 + r.nextInt(4);
 				if (operationtype != 5){
 					mixed = 0;
 				}
 
+				// store operators + print expression
 				if (operationtype == 1 || mixed == 1){
 					operators.push('+');
-					System.out.print(" + " + randomNum1) ;
+					System.out.print(" + " + randomNum) ;
 				}
 				if (operationtype == 2 || mixed == 2){
 					operators.push('-');
-					System.out.print(" - " + randomNum1) ;
+					System.out.print(" - " + randomNum) ;
 				}
 
+				// multiplication / division applied immediately (higher precedence)
 				double top1 = numbers.peek();
 				if (operationtype == 3 || mixed == 3){
-					System.out.print(" * " + randomNum1) ;
+					System.out.print(" * " + randomNum) ;
 					numbers.pop();
 					double top2 = numbers.peek();
 					numbers.pop();
 					numbers.push(top2*top1);
 				}
 				if (operationtype == 4 || mixed == 4){
-					System.out.print(" / " + randomNum1) ;
+					System.out.print(" / " + randomNum) ;
 					numbers.pop();
 					double top2 = numbers.peek();
 					numbers.pop();
 					numbers.push(top2/top1);
 				}
 			}	
-			System.out.println("\n" + numbers);	
-			System.out.println(operators);
-
 			
-			
+			// ---------------- FINAL EVALUATION ( + and - ) ----------------
 			while(numbers.size()>1){
 				Character operator = operators.peekLast();
 				double top1 = numbers.peekLast();
@@ -198,48 +207,68 @@ public class PracticeProblem {
 				}
 				operators.removeLast();
 			}
-					
-			System.out.println(numbers);	
-			System.out.println(operators);
+			
 			System.out.println(" = ?");
-			
-			double polarity = numbers.peek() * -1;
-			
-			double answer = polarity;
+
+			// ---------------- REVERSE ANSWER RULE ----------------
+			double answer = numbers.peek();
+
+			// round to maximum 3 decimals
             answer = Math.round(answer * 1000.0) / 1000.0;
-            String ans = Double.toString(answer);
-			String reverseans = "";
+
+			// reverse sign rule
+			String reversepolarity = "" ;
+			if (answer > 0){
+				reversepolarity = "-" ;
+			}
+			if (answer < 0){
+				answer = answer * -1;
+			}
+
+			// format number safely 
+            DecimalFormat df = new DecimalFormat("0.###");
+            String ans = df.format(answer);
+
+			// remove trailing .0
+			if (ans.endsWith(".0")) {
+				ans = ans.substring(0, ans.length() - 2);
+			}
+
+			// reverse digits
+			String reverseans = "" + reversepolarity;
 			for (int i2 = ans.length()-1 ; i2 >= 0 ; i2 --){
 				reverseans = reverseans + ans.charAt(i2);
 			}
 			double reverseans1 = Double.parseDouble(reverseans);
-			System.out.println(reverseans1);
 
-			System.out.println("Please enetr your answer: ");
+			// ---------------- USER INPUT CHECK ----------------
+			System.out.print("Please enter your answer: ");
 
 			while (!(ip.hasNextDouble())) {
 				ip.nextLine();
-			    System.out.println("Invalid Input!");
+			    System.out.println("\nInvalid Input!");
 			    System.out.print("Please enetr your answer: ");
 		    }
 
 			double playerans = ip.nextDouble();
 			ip.nextLine();
 
+			// score check
 			if (playerans == reverseans1){
 				score ++;
-				System.out.println("Congrats! This is the correct answer!");
+				System.out.println("\nCongrats! This is the correct answer!\n");
 			}
 			else{
-				System.out.println("This is incorrect, the correct answer is " + reverseans1);
+				System.out.println("\nThis is incorrect, the correct answer is " + reverseans1 + "\n");
 			}
 		}
-		System.out.println("You answered " + score + " out of " + rounds + " questions correctly");
+		// ---------------- FINAL SCORE ----------------
+		System.out.println("\nYou answered " + score + " out of " + rounds + " questions correctly.");
 		if (score > rounds/2){
-			System.out.println("Congrats! You are the ultimate boss of math");
+			System.out.println("Congrats! You are the ultimate boss of math!");
 		}
 		else{
-			System.out.println("You're lowkey fried, just restart your life lil bro");
+			System.out.println("You're lowkey fried, just restart your life lil bro.");
 		}
 
 	}
